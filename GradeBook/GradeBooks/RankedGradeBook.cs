fcc75433326095bedeb9b4435c1dd8a2824bd2a7
Viktor Butenko <viktor.butenko@microsoft.wsei.edu.pl>
2025-03-01 10:15:22 +0100

    GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,5 +12,34 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Students with less than 5 ");
+            }
+            var sortedGrades = Students.Select(s => s.AverageGrade).ToList();
+
+            sortedGrades.Sort();
+
+            sortedGrades.Reverse();
+
+            int studentsInputGrade = (int)Math.Ceiling(Students.Count * 0.2);
+            
+            int rank = sortedGrades.IndexOf(averageGrade);
+           
+            if (rank < studentsInputGrade)
+                return 'A';
+            else if (rank < studentsInputGrade * 2)
+                return 'B';
+            else if (rank < studentsInputGrade * 3)
+                return 'C';
+            else if (rank < studentsInputGrade * 4)
+                return 'D';
+            else
+                return 'F';
+
+        }
     }
 }
