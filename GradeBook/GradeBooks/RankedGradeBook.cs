fddecbbe12ed0da45a92533c434a99341df70091
Viktor Butenko <viktor.butenko@microsoft.wsei.edu.pl>
2025-03-01 10:34:43 +0100

    CalculateStatistics|CalculateStudentStatistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,7 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook (string name) : base(name)
+        public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
         }
@@ -26,9 +27,9 @@ namespace GradeBook.GradeBooks
             sortedGrades.Reverse();
 
             int studentsInputGrade = (int)Math.Ceiling(Students.Count * 0.2);
-            
+
             int rank = sortedGrades.IndexOf(averageGrade);
-           
+
             if (rank < studentsInputGrade)
                 return 'A';
             else if (rank < studentsInputGrade * 2)
@@ -39,6 +40,32 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else
                 return 'F';
+
+        }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.\" to the Console.");
+                return;
+            }
+            else if (Students.Count >= 5)
+            {
+                base.CalculateStatistics();
+            }
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            else if (Students.Count >= 5)
+            {
+                base.CalculateStudentStatistics(name);
+            }
+
 
         }
     }
